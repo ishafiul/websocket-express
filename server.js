51b8b4fc79902e14ecbd22a87992e7f57c0d3394
ishafiul <shafiulislam20@gmail.com>
2022-06-12 23:34:26 +0600

    connection and uid gen

--- server.js
+++ server.js
@@ -2,14 +2,37 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
+require("dotenv").config();
+
+const port = process.env.PORT || 8485;
 
 const wss = new WebSocket.Server({ server:server ,path:'/api/v1/ws/game1'});
 
-wss.on('connection', function connection(ws) {
-    console.log('A new client Connected!');
-    ws.send('Welcome New Client!');
+
+const clients = new Map();
+wss.on('connection', ws=> {
+    const id = uuidv4();
+    ws.send(id)
+    ws.on('message', (messageAsString) => {
+        const data = JSON.parse(messageAsString)
+        const username =data.clientId;
+        if (data.action === 'connect' ){
+            const metadata = { id, username};
+            clients.set(ws, metadata);
+            //console.log(clients.get(ws))
+        }
+    })
+    ws.on("close", () => {
+        clients.delete(ws);
+    });
 });
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-server.listen(8485, () => console.log(`Lisening on port :8485`))
+server.listen(port, () => console.log(`Lisening on port ${port}`))
