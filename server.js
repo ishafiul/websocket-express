c4a41ac8a9c2132cf8c3a52ffab7f406f926b032
ishafiul <shafiulislam20@gmail.com>
2022-06-12 21:01:45 +0600

    git ignore added

--- server.js
+++ server.js
@@ -8,11 +8,6 @@ const wss = new WebSocket.Server({ server:server ,path:'/api/v1/ws/game1'});
 wss.on('connection', function connection(ws) {
     console.log('A new client Connected!');
     ws.send('Welcome New Client!');
-    let i = 0
-    setInterval(()=>{
-        ws.send(i)
-        i++
-    },20)
 });
 
 app.get('/', (req, res) => res.send('Hello World!'))
