7833a6aeb6aebb8f9d54ebdaa2731ccd8342bd8c
ishafiul <shafiulislam20@gmail.com>
2022-06-14 01:02:37 +0600

    ws heartbeat added

--- server.js
+++ server.js
@@ -12,25 +12,46 @@ const wss = new WebSocket.Server({ server:server ,path:'/api/v1/ws/game1'});
 const clients = new Map();
 wss.on('connection', ws=> {
     const id = uuidv4();
-    ws.send(id)
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
     ws.on('message', (messageAsString) => {
         const data = JSON.parse(messageAsString)
         const username =data.username;
         if (data.action === 'connect' ){
             const metadata = { id, username};
+            ws.send(JSON.stringify({id,username}))
             clients.set(ws, metadata);
             console.log(clients.get(ws))
         }
+        if (data.action === 'testmessage'){
+            ws.send(JSON.stringify({message: `connection ok. you send ${data.message}`}))
+        }
     })
-    ws.on("close", () => {
-        clients.delete(ws);
-    });
+
 });
+
+const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+
+        ws.isAlive = false;
+        ws.ping();
+    });
+}, 300);
+
+
+wss.on("close", () => {
+    clearInterval(interval);
+});
+
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
+}
+function heartbeat() {
+    this.isAlive = true;
 }
 
 app.get('/', (req, res) => res.send('Hello World!'))
